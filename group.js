8c908f6b3aae144271ae1b9fb4c82d222d9882d6
Davidhaoy <102175533+Davidhaoy@users.noreply.github.com>
2022-04-21 21:59:26 -0400

    Add files via upload

---
+++ group.js
@@ -0,0 +1,65 @@
+function davidD(){
+	document.getElementById('davidthreehundred').innerHTML ='工作室香港和新加坡合作，新闻启动学院，一个教育公司在香港，组织人物工作室为8至14岁的学生。在两个小时的课程中，20名学生聚集在拳击比赛香港办公室，学习如何从零开始创造一个角色。学生们在以Ionia、frejord、shadow island和piltwalf为主题的教室里分组工作，使用两套专注于外观和背景故事的表格。Fist Illustrator展示了如何赋予他们的创造力和草图生命。课程结束后，学生们可以把新的专业手绘角色带回家。我相信你可以猜到工作室的气氛可以说是高潮。'
+}
+function davidA(){
+	document.getElementById('davidfivehundred').innerHTML ='上海办事处与上海穗首盟手语文化中心合作举办了为期两天的手语培训营。两名教师和一名翻译向员工介绍了聋人文化，并进行了基本的上海手语教学。共有48名fist员工参加了课程，气氛非常热烈。许多学生还报名参加了文化中心聋儿服务项目，准备在业余时间参加中心的学习和游戏活动。'
+}
+function davidV(){
+	document.getElementById('davidthree').innerHTML ='Fist员工被邀请在全球服务月期间向该组织捐赠温暖的冬衣。受新冠肺炎经济的影响，巴西圣保罗的无家可归者人数有所增加。对于每一件捐赠的衣物，fist Brazic office承诺向非营利组织捐赠150巴西雷亚尔，帮助他们为更多无家可归者准备淋浴和清洁衣物。最终捐款总额超过1.3万巴西雷亚尔（约2500美元），可帮助圣保罗近1300名无家可归者。'
+}
+function e(){
+	document.getElementById('a').innerHTML='特战英豪';
+	document.getElementById('a').style.textShadow ='3px 2px 3px lightgoldenrodyellow';
+}
+function sum(){
+	var a = prompt('first name:');
+	var b = prompt('second name:');
+	var c = ('Nice to meet you ')
+	document.getElementById('h').innerHTML=c+a+b;
+	document.getElementById('h').style.color ='white'
+}
+function i(){
+	document.getElementById('f').innerHTML ='在被世人遺忘的院落中，敵對特務爭相搶攻三處據點，你能聽到呼喊聲此起彼落。雖然還有其他地點須得留心，但守備方不妨善用多出來的據點，來積極推進攻勢。';
+}
+function j(){
+	document.getElementById('k').innerHTML ='在被世人遺忘的院落中，敵對特務爭相搶攻三處據點，你能聽到呼喊聲此起彼落。雖然還有其他地點須得留心，但守備方不妨善用多出來的據點，來積極推進攻勢。';
+}
+function k(){
+	document.getElementById('l').innerHTML ='兩個據點，沒有中路，只能選擇向左或向右。結果會怎麼樣呢？兩處據點都有通道讓攻擊方可以長驅直入，配上兩道單向傳送門，讓夾擊戰略更容易施展。';
+}
+function l(){
+	document.getElementById('m').innerHTML ='這間祕密研究機構因為一場失敗的輻能實驗而裂成了兩半。如同地圖的設計，守備方的選擇也分成兩種：你要直接進擊，在攻擊方的地盤上正面對戰，還是做好十足準備，被動迎戰呢？';
+}
+function u(){
+	document.getElementById('o').innerHTML='特工';
+	document.getElementById('o').style.textShadow ='3px 2px 3px lightgoldenrodyellow';
+}
+function change(){
+document.getElementById('fryone').innerHTML = '(Teamfight Tactics)，(League of Legends: Wild Rift)，(Legends of Runeterra)，(VALORANT)，(Ruined King: A League of Legends Story)';
+
+}
+function picture(){
+ document.getElementById('frytwo').src ='http://5b0988e595225.cdn.sohucs.com/images/20190803/d080183a14fb4d64af5ac70d9124fe76.jpeg';
+}
+
+
+
+function para(){
+ var css = document.getElementById('fryone').style;
+ css.backgroundColor ='lightgreen';
+ css.fontFamily ='arial';
+ 
+}
+function sum(){
+ var a = prompt('first name:');
+ var b = prompt('second name:');
+ var c = ('Welcome to Game Country ')
+
+ document.getElementById('fryfive').innerHTML=c+a+b;
+}
+function tonypara(){
+ var css = document.getElementById('tonythree').style;
+ css.backgroundColor ='blue';
+ css.fontFamily ='arial';
+ css.textAlign ='center'
+}
